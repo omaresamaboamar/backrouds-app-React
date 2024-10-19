6ea67da04838e76b8c9ef2b2e885ecfcab111690
omaresamaboamar <omaresamaboamar@gmail.com>
2024-10-19 23:37:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import About from "./Componentes/About";
+import Footer from "./Componentes/Footer";
+import Hero from "./Componentes/Hero";
+import Navbar from "./Componentes/Navbar";
+import Services from "./Componentes/Services";
+import Tours from "./Componentes/Tours";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Services.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Title from "./Title";
+import { Service } from "../data";
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subtitle="services" />
+      <div className="section-center services-center">
+        {Service.map((service) => {
+          const { id, title, icon, text } = service;
+          return (
+            <article className="service" key={id}>
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
